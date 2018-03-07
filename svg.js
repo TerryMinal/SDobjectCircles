6a554315e075a75b4727111f4d4692332a5a108e
srafi1 <shakilrafi0@gmail.com>
2018-03-07 13:47:03 -0500

    done?

--- svg.js
+++ svg.js
@@ -1,45 +1,46 @@
 var pic = document.getElementById("vimage");
-var clearBtn = document.getElementById("clearBtn")
+var clearBtn = document.getElementById("clear")
 var dots = [];
 
 //clears the screen
 var clearScreen = function(e){
-    pic.innerHTML = "";
+    dots.forEach((a) => a.element.remove());
     console.log("Cleared Screen.")
 }
 
 //a circle has been clicked -- two paths: Change or Die
 var circle_clicked = function(e){
-    if (this.getAttribute('fill') == 'red') {
-        this.setAttribute('fill', 'green');
+    if (this.getAttribute('fill') == 'green') {
+        this.setAttribute('fill', 'red');
         this.setAttribute('cx', Math.random() * 485 + 15);
         this.setAttribute('cy', Math.random() * 485 + 15);
     } else {
-        this.setAttribute('fill', 'red');
+        this.setAttribute('fill', 'green');
     }
 }
 
-//draws a circle at the specified location
-var drawDot = function(x,y){
+var makeDot = function(x, y) {
     var cl = document.createElementNS("http://www.w3.org/2000/svg", "circle");
     cl.setAttribute("cx", x);
     cl.setAttribute("cy", y);
     cl.setAttribute("r", "15");
-    cl.setAttribute("fill", "green");
+    cl.setAttribute("fill", "red");
     pic.appendChild(cl);
-    //add event listener to circle
     cl.addEventListener('click', circle_clicked);
-    return
+    return {
+        x : x,
+        y : y,
+        fill : "red",
+        stroke : "red",
+        element : cl
+    };
 }
 
 //a blank space on svg container is clicked -- make circle
 var svg_clicked = function(e){
-    console.log(e.target);
-    console.log(this);
-    console.log(e.target == this);
     if (e.target == this){  //<-- toElement is undefined
         console.log("coords: ", e.offsetX, ", ", e.offsetY);
-        drawDot(e.offsetX,e.offsetY);
+        dots.push(makeDot(e.offsetX,e.offsetY));
     };
 }
 
